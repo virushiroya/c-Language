21498c5ecafa5ac40bd414f67bd3578b2ea0311b
viroo <145561418+virushiroya@users.noreply.github.com>
2023-11-08 16:37:57 +0530

    7-11-2023

--- C_LANGUAGE/tempCodeRunnerFile.c
+++ C_LANGUAGE/tempCodeRunnerFile.c
@@ -1,58 +1,22 @@
 #include<stdio.h>
 
 int main()
-
 {
-
-   int eng[5],guj[5],stat[5],total[5],per[5];
-   int i;
-   char grade[5];
-
-	for(i=0; i < 5; i++)
-	{
-		printf("\nEnter %d students marks \n",i+1);
-		printf("\nEnter English marks : ");
-		scanf("%d",&eng[i]);
-		printf("\nEnter Gujrati marks : ");
-		scanf("%d",&guj[i]);
-		printf("\nEnter stat marks : ");
-		scanf("%d",&stat[i]);
-		total[i]=eng[i] + guj[i] + stat[i];
-		per[i]=(total[i]*100)/300;
-	}
-
-	printf("\nNo.\teng\tguj\tstat\ttotal\tper\tgrade\t\n");
-	for(i=0; i < 5; i++)
-	{
-		printf("\n%d\t%d\t%d\t%d\t%d\t%d\t",i+1,eng[i],guj[i],stat[i],total[i],per[i],grade[i]);
-
-
-		if(per[i]<=35)
-		{
-			printf("fail");
-		}
-		else if(per[i]>=90)
-		{
-			printf(" Grade A ");
-		}
-		else if(80<=per[i]<90)
-		{
-			printf(" Grade B ");
-		}
-		else if(per[i]>=70 && per[i]<80)
-		{
-			printf(" Grade C ");
-		}
-		else if(60<=per[i]<70)
-		{
-			printf(" Grade D ");
-		}
-		else
-		{
-			printf(" Grade E ");
-		}
-
-	}
-
-		return 0;	
+    int a[10]={1,2,3,4,5,6,7,8};
+    for(int i=0;i<8;i++)
+    {
+        if(i<3)
+        {
+            a[i]=a[i];
+        }else if(i>=4)
+        {
+            a[i]=a[i+1];
+        }
+    }
+    a[2]=6;
+    for(int i=0;i<6;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    return 0;
 }
